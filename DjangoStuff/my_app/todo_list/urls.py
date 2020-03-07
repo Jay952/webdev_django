53226d26343261ddfd496e16d64ce0a640deff03
[Jay952] <[jaydeep4420@gmail.com]>
2020-03-07 18:34:01 +0530

    Third commit

--- DjangoStuff/my_app/todo_list/urls.py
+++ DjangoStuff/my_app/todo_list/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
    path('', views.home, name='home'),
-   path('Jay', views.Jay, name='Jay'),
+   path('about/', views.about, name='about'),
   
 ]
+	
--- DjangoStuff/my_app/todo_list/views.py
+++ DjangoStuff/my_app/todo_list/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request, 'home.html',{}),
+	return render(request, 'home.html',{})
 
-def Jay(request):
+def about(request):
 
     return render(request, 'about.html',{})
 
